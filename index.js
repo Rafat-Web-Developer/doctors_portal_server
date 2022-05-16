e8e4362af460d08af2dfd004b1d4bc1b2645e296
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-16 12:16:04 +0600

    Create available api and check validation in post service api

--- index.js
+++ index.js
@@ -37,10 +37,53 @@ async function run() {
       res.send(services);
     });
 
+    // Warning: This is not the proper way to query multiple collection.
+    // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+
+      // step 1:  get all services
+      const services = await servicesCollection.find().toArray();
+
+      // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+      const query = { date: date };
+      const bookings = await bookingsCollection.find(query).toArray();
+
+      // step 3: for each service
+      services.forEach((service) => {
+        // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+        const serviceBookings = bookings.filter(
+          (book) => book.treatment_name === service.name
+        );
+        // step 5: select slots for the service Bookings: ['', '', '', '']
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+        // step 6: select those slots that are not in bookedSlots
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        //step 7: set available to slots to make it easier
+        service.slots = available;
+      });
+
+      res.send(services);
+    });
+
     app.post("/booking", async (req, res) => {
       const booking = req.body;
+      const query = {
+        treatment_name: booking.treatment_name,
+        date: booking.date,
+        patientEmail: booking.patientEmail,
+      };
+      const exist = await bookingsCollection.findOne(query);
+      if (exist) {
+        return res.send({
+          success: false,
+          message: "You already already have an appointment on this slot.",
+        });
+      }
       const result = await bookingsCollection.insertOne(booking);
-      res.send(result);
+      res.send({ success: true, result });
     });
     // ----->All API End<-----
   } finally {
